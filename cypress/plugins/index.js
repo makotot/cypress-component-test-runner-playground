3f9bbd940c0eeb2d6a770e5a33f69565c3bcb458
makotot <makoto.ttn@gmail.com>
2021-05-03 01:29:18 +0900

    imp

---
+++ cypress/plugins/index.js
@@ -0,0 +1,9 @@
+import injectDevServer from '@cypress/react/plugins/react-scripts';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (on, config) {
+  if (config.testingType === 'component') {
+    injectDevServer(on, config);
+  }
+  return config;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react';
+import { Scrollspy } from './Scrollspy';
+
+const SIZE = 5;
 
 function App() {
+  const sectionRefs = [
+    useRef<HTMLDivElement>(null),
+    useRef<HTMLDivElement>(null),
+    useRef<HTMLDivElement>(null),
+    useRef<HTMLDivElement>(null),
+    useRef<HTMLDivElement>(null),
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Scrollspy sectionRefs={sectionRefs}>
+        {({ currentElementIndexInViewport }) => (
+          <div>
+            <ul
+              data-cy="nav-wrapper"
+              style={{
+                listStyle: 'none',
+                position: 'fixed',
+                top: 0,
+                right: 0,
+                backgroundColor: '#fff',
+                padding: '1rem',
+              }}>
+              {new Array(SIZE).fill(0).map((_, i) => (
+                <li key={i}>
+                  <a
+                    href={`#section-${i}`}
+                    data-cy={`nav-item`}
+                    className={
+                      currentElementIndexInViewport === i ? 'active' : ''
+                    }
+                    style={{
+                      color:
+                        currentElementIndexInViewport === i ? '#f00' : '#222',
+                    }}>
+                    section-{i}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <div data-cy="section-wrapper">
+              {new Array(SIZE).fill(0).map((_, i) => (
+                <div
+                  id={`section-${i}`}
+                  data-cy={`section-item`}
+                  key={i}
+                  ref={sectionRefs[i]}
+                  className={
+                    currentElementIndexInViewport === i ? 'active' : ''
+                  }
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '500px',
+                    backgroundColor: `#${i}${i}${i}`,
+                    color: '#fff',
+                    fontSize: '2rem',
+                  }}>
+                  {i}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </Scrollspy>
     </div>
   );
 }
